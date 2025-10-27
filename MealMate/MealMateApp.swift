ad9d4efd4cdbab951845aafc06543b08fe05d6cd
sangampokharel803@gmail.com <sangampokharel803@gmail.com>
2025-10-27 13:00:00 +0545

    Initial Commit

---
+++ MealMate/MealMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealMateApp.swift
+//  MealMate
+//
+//  Created by sangam pokharel on 27/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct MealMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
